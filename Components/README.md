d799f447cc721bf5727e9040004145d6bcd6db0a
angelicrf <angelicrf@gmail.com>
2023-01-24 13:56:01 -0500

    test-send-email

--- Components/README.md
+++ Components/README.md
@@ -1,3 +1,3 @@
 Web-Url:
 
-    https://angelicrf.github.io/bs5-boatusers/Components/Home.html
+   https://angelicrf.github.io/bs5-boatusers/index.html
--- Scripts/search.js
+++ Scripts/search.js
@@ -3,11 +3,46 @@ const clickSearch = () => {
 
     getSearch.value.trim().length != 0 ? (setSearchValue(getSearch.value), getSearch.value = "") : alert("search value is empty!")
 }
-
-
 const setSearchValue = (thisValue) => {
 
     let getSearchValue = document.getElementById("searchValue")
 
     getSearchValue.innerHTML = "searchvalue: " + thisValue
+}
+const emailInfo = () => {
+
+    console.log('btn clicked')
+    let thisBody = JSON.stringify({
+        "Messages": [
+            {
+                "From": {
+                    "Email": "angelicrf@gmail.com",
+                    "Name": "ClientName"
+                },
+                "To": [
+                    {
+                        "Email": "angelicrf@gmail.com",
+                        "Name": "BoatUsers"
+                    }
+                ],
+                "Subject": "My first BoatUsers Email!",
+                "TextPart": "Greetings from BoatUsers!",
+                "HTMLPart": "<h3>Dear Client,</h3><br />Your Email will be sent asap!"
+            }
+        ]
+    })
+
+    fetch("https://api.mailjet.com/v3.1/send", {
+        method: 'POST',
+        body: thisBody,
+        //credentials: 'same-origin',
+        header: {
+            "Content-Type": "application/json",
+            'Authorization': 'Basic ' + btoa('6cd68fea322c1e176923b684b4aa43d8:abf29ab2bd05fac2c4af40387302c130')
+        }
+        //auth: { username: '6cd68fea322c1e176923b684b4aa43d8', password: 'abf29ab2bd05fac2c4af40387302c130' },
+    })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
