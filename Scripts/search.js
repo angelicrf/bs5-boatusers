85ee67488bce52efd3d3b0832382f44127449c5b
angelicrf <angelicrf@gmail.com>
2023-01-24 18:02:02 -0500

    btoa-fetch

--- Scripts/search.js
+++ Scripts/search.js
@@ -40,7 +40,7 @@ const emailInfo = () => {
             //"Access-Control-Allow-Origin": "*",
             //"Accept": 'application/json',
             "Content-Type": "application/json",
-            "Authorization": "Basic " + base64.encode('6cd68fea322c1e176923b684b4aa43d8:abf29ab2bd05fac2c4af40387302c130')
+            "Authorization": "Basic " + btoa('6cd68fea322c1e176923b684b4aa43d8:abf29ab2bd05fac2c4af40387302c130')
         }
         //auth: { username: '6cd68fea322c1e176923b684b4aa43d8', password: 'abf29ab2bd05fac2c4af40387302c130' },
     })
