652d4773733c238e0d18b30c9143b4c1c06c2e8d
angelicrf <angelicrf@gmail.com>
2023-01-24 21:41:20 -0500

    basic-auth-mdf

--- Scripts/search.js
+++ Scripts/search.js
@@ -12,6 +12,35 @@ const setSearchValue = (thisValue) => {
 const emailInfo = () => {
 
     console.log('btn clicked')
+    /*  curl -s \
+         -X POST \
+         --user "6cd68fea322c1e176923b684b4aa43d8:abf29ab2bd05fac2c4af40387302c130" \
+         https://api.mailjet.com/v3.1/send \
+        -H 'Content-Type: application/json' \
+         -H 'Access-Control-Allow-Credentials: true' \
+         -H  'Access-Control-Allow-Origin: https://angelicrf.github.io/bs5-boatusers' \
+         -H 'Access-Control-Request-Method: POST' \
+         -H 'Access-Control-Request-Headers: *' \
+         -d '{
+             "Messages": [
+                 {
+                     "From": {
+                         "Email": "angelicrf@gmail.com",
+                         "Name": "Me"
+                     },
+                     "To": [
+                         {
+                             "Email": "angelicrf@gmail.com",
+                             "Name": "You"
+                         }
+                     ],
+                     "Subject": "My first BoatUsers Email!",
+                     "TextPart": "Greetings from BoatUsers!",
+                     "HTMLPart": "<h3>Dear Client,</h3><br />Your Email will be sent asap!"
+                 }
+             ]
+         }'*/
+
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify({
@@ -51,11 +80,11 @@ const emailInfo = () => {
     myHeaders.append("Accept", "application/json, text/plain, */*")
     myHeaders.append("Access-Control-Allow-Methods", "POST")
     myHeaders.append("Access-Control-Allow-Headers", "*")
-    myHeaders.append("access-control-request-headers", "*")
-    myHeaders.append("access-control-request-method", "POST")
+    myHeaders.append("Access-control-request-headers", "*")
+    myHeaders.append("Access-control-request-method", "POST")
     myHeaders.append("Access-Control-Allow-Credentials", "true")
     myHeaders.append("Content-Type", "text/html")
-    myHeaders.append("Authorization", "Basic " + btoa('6cd68fea322c1e176923b684b4aa43d8:abf29ab2bd05fac2c4af40387302c130'))
+    myHeaders.append("Authorization", "Basic NmNkNjhmZWEzMjJjMWUxNzY5MjNiNjg0YjRhYTQzZDg6YWJmMjlhYjJiZDA1ZmFjMmM0YWY0MDM4NzMwMmMxMzA=")
 
     fetch("https://api.mailjet.com/v3.1/send", {
         method: 'POST',
